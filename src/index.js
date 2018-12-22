71f94db925aeb1e2cce677afd41dbf72aa3149a8
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-12-21 18:20:07 -0800

    Create new ApprovalCard component and refactor

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Comment from './Comment';
+import ApprovalCard from './ApprovalCard';
 import faker from 'faker';
 
 const App = () => {
 
     return (
         <div className="ui container comments">
-            <Comment
-            author={faker.name.firstName()}
-            datetime={faker.date.past().toLocaleString()}
-            content={faker.lorem.sentence()}
-            avatar={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <Comment
+                    author={faker.name.firstName()}
+                    datetime={faker.date.past().toLocaleString()}
+                    content={faker.random.words()}
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     
     
