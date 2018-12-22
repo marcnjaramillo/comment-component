ff53939448ad70c3e1a73a84df0c099014d5c6a9
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-12-21 17:23:55 -0800

    Refactor and implement faker.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Comment from './Comment';
 import faker from 'faker';
 
 const App = () => {
+
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00 PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
-        
+            <Comment
+            author={faker.name.firstName()}
+            datetime={faker.date.past().toLocaleString()}
+            content={faker.lorem.sentence()}
+            avatar={faker.image.avatar()}
+            />
         </div>
     
     
